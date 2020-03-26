8bc407a2a46ab12da70522f665bbc0bd34d0b840
natan <eliasnatanzigaran@hotmail.com>
2020-03-26 19:48:09 -0300

    Cambios en el navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import './NavBar.css'
 
 class NavBar extends Component {
+
+  // const navBtn = document.getElementById('nav');
+
+  handleClick = e => {
+    const navBtn = document.getElementById('nav')
+    if (navBtn.classList.contains('navbarBg')) {
+      navBtn.classList.remove('navbarBg')
+    } else { navBtn.classList.add('navbarBg') }
+  }
+
   render() {
     return (
-      <nav class="navbar navbar-expand-md navbar-dark">
+      <nav class="navbar navbar-expand-md navbar-dark" id="nav">
         <a class="navbar-brand" href="#">ZIGARAN ELIAS</a>
 
-        <button class="btn btn-sm btn-outline-secondary navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="btn btn-sm btn-outline-secondary navbar-toggler" type="button" onClick={this.handleClick} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
