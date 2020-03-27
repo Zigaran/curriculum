27807a15b58ad933d94ee67a406e627b3c43224a
Zigaran <eliasnatanzigaran@hotmail.com>
2020-03-27 17:09:56 -0300

    First Page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,32 +14,32 @@ class NavBar extends Component {
 
   render() {
     return (
-      <nav class="navbar navbar-expand-md navbar-dark" id="nav">
-        <a class="navbar-brand" href="#">ZIGARAN ELIAS</a>
+      <nav className="navbar navbar-expand-md navbar-dark" id="nav">
+        <a className="navbar-brand" href="#">ZIGARAN ELIAS</a>
 
-        <button class="btn btn-sm btn-outline-secondary navbar-toggler" type="button" onClick={this.handleClick} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="btn btn-sm btn-outline-secondary navbar-toggler" type="button" onClick={this.handleClick} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Education</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Education</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Skills</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Skills</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Portfolio</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfolio</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Off-Topic</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Off-Topic</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
             </li>
 
 
--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -5,9 +5,18 @@ import profilePic from '../images/profilePic.png';
 class Perfil extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <div className="">
-                    <img src={profilePic} className="profilePic" ></img>
+                    <img src={profilePic} className="picSize" alt="Foto de perfil"></img>
+                </div>
+
+                <div className="profileDesc">
+                    <h1 className="desc">Zigaran Elias</h1>
+                    <h6 className="desc">ENGINEERING STUDENT, FRONTEND DEVELOPER, PIANIST</h6>
+                </div>
+
+                <div className="">
+                    <button type="button" className="btn-dark btn-lg">Download CV</button>
                 </div>
             </div>
         );
