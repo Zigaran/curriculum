7c39e09bd7b908ba2c4d2d7f703535dd1e4b9b30
Zigaran <eliasnatanzigaran@hotmail.com>
2020-03-31 13:50:27 -0300

    Updates

---
+++ precache-manifest.8ef48734a063c0bbe81041fa05c86df6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e043d6adf9c21ed32073234bb8b7f9f4",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "9b72bc57508b8a8a1387",
+    "url": "/curriculum/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8879265bf636e99b1a80",
+    "url": "/curriculum/static/css/main.fff85b62.chunk.css"
+  },
+  {
+    "revision": "9b72bc57508b8a8a1387",
+    "url": "/curriculum/static/js/2.d0deaafa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curriculum/static/js/2.d0deaafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8879265bf636e99b1a80",
+    "url": "/curriculum/static/js/main.3c83e78f.chunk.js"
+  },
+  {
+    "revision": "c4760d44694e62a78808",
+    "url": "/curriculum/static/js/runtime-main.4e046246.js"
+  },
+  {
+    "revision": "03f203769fd5a235846c792ee9e5ecbd",
+    "url": "/curriculum/static/media/instagram.03f20376.png"
+  },
+  {
+    "revision": "93ab66b05386cdabf908a991a3e5d515",
+    "url": "/curriculum/static/media/logotipo-de-github.93ab66b0.png"
+  },
+  {
+    "revision": "62828906cda80aa0134af575561366ed",
+    "url": "/curriculum/static/media/profilePic.62828906.png"
+  }
+]);
