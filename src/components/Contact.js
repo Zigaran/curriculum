b6609847a80a1b006767491033f93d5a8cd08e9a
Zigaran <zigaraneliasnatan@gmail.com>
2020-04-14 18:37:50 -0300

    BACKUP

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+class Contact extends Component {
+    render() {
+        return (
+
+    );
+    }
+
+}
+
+export default Contact;
